d544942ced16116a5a51345fbe8d7b4f9b250d3a
Khalifa Ahmad <ahmadkhalifa484@gmail.com>
2025-11-29 08:46:35 +0100

    trying to fix db ssl and connectivity issues

--- app/(common)/lib/db.js
+++ app/(common)/lib/db.js
@@ -1,9 +1,11 @@
 //
 import mysql from 'mysql2/promise';
-import fs from 'fs'; // node file system
+// import fs from 'fs'; // node file system
 
-const isProduction = process.env.NODE_ENV === 'production';
-const caPath = process.env.DB_CA_PATH;
+// const isProduction = process.env.NODE_ENV === 'production';
+// const caPath = process.env.DB_CA_PATH;
+
+const isLocal = process.env.NODE_ENV === 'development';
 
 const baseConfig = mysql.createPool({
   host: process.env.DB_HOST,
@@ -16,19 +18,41 @@ const baseConfig = mysql.createPool({
 //     rejectUnauthorized: true
 //   }
 
-// Conditionally configure SSL only when the CA file path is provided
-if (isProduction && caPath) {
+let finalConfig = { ...baseConfig };
+
+// // Conditionally configure SSL only when the CA file path is provided
+// if (isProduction && caPath) {
+//     try {
+//         baseConfig.ssl = {
+//             rejectUnauthorized: true, // Requires certificate validation
+//             // Read the CA file synchronously from the server's file system at startup
+//             ca: fs.readFileSync(caPath) 
+//         };
+//     } catch (error) {
+//         console.error("CRITICAL: Failed to load SSL CA certificate from path:", caPath, error);
+//         // 
+//         throw new Error("Missing critical SSL certificate for production database connection.");
+//     }
+// }
+
+if (!isLocal) {
+    // This runs in prod
     try {
-        baseConfig.ssl = {
-            rejectUnauthorized: true, // Requires certificate validation
-            // Read the CA file synchronously from the server's file system at startup
-            ca: fs.readFileSync(caPath) 
+        const fs = require('fs'); // NodeFS module is available in prodserver env
+        finalConfig.ssl = {
+            rejectUnauthorized: true, // Requires cert validation
+            ca: fs.readFileSync(process.env.DB_CA_PATH) // Path set in DO secrets
         };
     } catch (error) {
-        console.error("CRITICAL: Failed to load SSL CA certificate from path:", caPath, error);
-        // 
+        console.error("CRITICAL: Failed to load SSL CA certificate:", error);
+        // Throwing this error stops the production server from starting insecurely
         throw new Error("Missing critical SSL certificate for production database connection.");
     }
+} else {
+    // This runs in development (Local machine)
+    // We explicitly avoid setting finalConfig.ssl, disabling SSL for the local connection.
 }
 
-export const pool = mysql.createPool(baseConfig);
+// export const pool = mysql.createPool(baseConfig);
+
+export const pool = mysql.createPool(finalConfig);
